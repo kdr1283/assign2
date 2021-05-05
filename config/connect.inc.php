7aecdd4a22420602be87d701dfeb5933da884178
Rafael <ramos.rafael42@gmail.com>
2021-05-06 11:39:44 +1200

    simple ajax

---
+++ config/connect.inc.php
@@ -0,0 +1,7 @@
+<?php
+    // Information and credentials for connecting to the database
+    define("host", "cmslamp14.aut.ac.nz");
+    define("username", "kdr1283");    
+    define("password", "jacenice123"); 
+    define("database", "kdr1283");
+?>
